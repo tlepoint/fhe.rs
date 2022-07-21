1e3a6c2db6cc325967a57fd5d68479544a97e680
Tancrède Lepoint <tlepoint@users.noreply.github.com>
2022-07-21 14:44:08 -0400

    Format

--- crrust-math/src/rns/extender.rs
+++ crrust-math/src/rns/extender.rs
@@ -103,7 +103,8 @@ impl RnsExtender {
 			&self.q_mod_p,
 			&self.q_mod_p_shoup,
 		) {
-			let mut x = (2 * p_j.modulus() - p_j.lazy_mul_shoup(value as u64, *q_mod_p_j, *q_mod_p_shoup_j)) as u128;
+			let mut x = (2 * p_j.modulus()
+				- p_j.lazy_mul_shoup(value as u64, *q_mod_p_j, *q_mod_p_shoup_j)) as u128;
 			for (yi, q_star_mod_p_j_i, q_star_mod_p_shoup_j_i) in
 				izip!(&y, q_star_mod_p_j, q_star_mod_p_shoup_j,)
 			{
