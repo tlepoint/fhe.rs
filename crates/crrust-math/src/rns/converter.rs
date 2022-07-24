a057dca20f5eae91d338f2c91827bae983b9995e
Tancrède Lepoint <tlepoint@users.noreply.github.com>
2022-07-23 23:01:49 -0400

    Use u256

--- crates/crrust-math/src/rns/converter.rs
+++ crates/crrust-math/src/rns/converter.rs
@@ -8,6 +8,7 @@ use crate::{rns::i193::Int193, zq::Modulus};
 use itertools::izip;
 use num_bigint::BigUint;
 use num_traits::ToPrimitive;
+use primitive_types::U256;
 
 /// Converter from one RNS basis to another.
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -78,7 +79,8 @@ impl RnsConverter {
 		let mut rests_to = Vec::with_capacity(self.to.moduli_u64.len());
 
 		let mut y = Vec::with_capacity(rests_from.len());
-		let mut sum = Int193::zero();
+		// let mut sum = Int193::zero();
+		let mut sum2 = U256::zero();
 		for (rests_from_i, theta_lo, theta_hi, q_tilde, q_tilde_shoup, qi) in izip!(
 			rests_from,
 			&self.theta_lo,
@@ -92,10 +94,14 @@ impl RnsConverter {
 			// Compute yi * theta
 			let lo = (*theta_lo as u128) * (yi as u128);
 			let hi = (*theta_hi as u128) * (yi as u128) + (lo >> 64);
-			sum.add(lo as u64, hi as u64, (hi >> 64) as u64, false);
+			// sum.add(lo as u64, hi as u64, (hi >> 64) as u64, false);
+			sum2 += (U256::from(hi) << 64) + lo;
 		}
-		sum >>= 128;
-		let value: i128 = (&sum).into();
+		// sum >>= 128;
+		// let value: i128 = (&sum).into();
+		sum2 >>= 128;
+		let value2 = sum2.as_u64();
+		// assert_eq!(value as u64, value2);
 
 		for (q_star_mod_p_j, q_star_mod_p_shoup_j, p_j, q_mod_p_j, q_mod_p_shoup_j) in izip!(
 			&self.q_star_mod_p,
@@ -105,7 +111,7 @@ impl RnsConverter {
 			&self.q_mod_p_shoup,
 		) {
 			let mut x = (2 * p_j.modulus()
-				- p_j.lazy_mul_shoup(value as u64, *q_mod_p_j, *q_mod_p_shoup_j)) as u128;
+				- p_j.lazy_mul_shoup(value2, *q_mod_p_j, *q_mod_p_shoup_j)) as u128;
 			izip!(&y, q_star_mod_p_j, q_star_mod_p_shoup_j).for_each(
 				|(yi, q_star_mod_p_j_i, q_star_mod_p_shoup_j_i)| {
 					x += p_j.lazy_mul_shoup(*yi, *q_star_mod_p_j_i, *q_star_mod_p_shoup_j_i) as u128
