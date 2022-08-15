8ea4c4d550ed21b9b86dc9d6c27314dd364049df
Tancrède Lepoint <tlepoint@users.noreply.github.com>
2022-08-15 13:05:01 -0400

    Small refactoring of MulPIR (#71)
    * Small refactoring of MulPIR
    
    * Running cargo fmt

--- crates/fhers/examples/mulpir/main.rs
+++ crates/fhers/examples/mulpir/main.rs
@@ -1,80 +1,16 @@
 #![feature(int_log)]
 #![feature(int_roundings)]
 
+mod common;
+
 use bfv::{traits::*, *};
+use common::{encode_database, generate_database, number_elements_per_plaintext};
 use indicatif::HumanBytes;
 use itertools::izip;
-use ndarray::{Array2, Axis};
+use ndarray::Axis;
 use rand::{thread_rng, RngCore};
-use std::{fmt, sync::Arc, time::Duration};
-use util::{transcode_backward, transcode_forward};
-
-// Utility for displaying duration
-pub struct DisplayDuration(pub Duration);
-
-impl fmt::Display for DisplayDuration {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		let duration_us = (self.0.as_secs_f64() * 1000000.0).round();
-		if duration_us >= 1000.0 {
-			write!(f, "{} ms", (duration_us / 100.0).round() / 10.0)
-		} else {
-			write!(f, "{} us", duration_us)
-		}
-	}
-}
-
-// Utility macro
-macro_rules! timeit {
-	($name:expr, $code:expr) => {{
-		let start = std::time::Instant::now();
-		let r = $code;
-		println!("⏱  {}: {}", $name, DisplayDuration(start.elapsed()));
-		r
-	}};
-}
-
-// Utility functions for PIR
-fn generate_database(database_size: usize, elements_size: usize) -> Vec<Vec<u8>> {
-	let mut database = vec![vec![0u8; elements_size]; database_size];
-	for i in 0..database.len() {
-		database[i][..4].copy_from_slice(&(i as u32).to_le_bytes());
-	}
-	database
-}
-
-fn number_elements_per_plaintext(par: &Arc<BfvParameters>, elements_size: usize) -> usize {
-	(par.plaintext().ilog2() as usize * par.degree()) / (elements_size * 8)
-}
-
-fn encode_database(database: &Vec<Vec<u8>>, par: &Arc<BfvParameters>) -> Array2<Plaintext> {
-	let elements_size = database[0].len();
-	let number_elements_per_plaintext = number_elements_per_plaintext(par, elements_size);
-	let number_rows = database.len().div_ceil(number_elements_per_plaintext);
-	println!("number_rows = {}", number_rows);
-	println!(
-		"number_elements_per_plaintext = {}",
-		number_elements_per_plaintext
-	);
-	let dimension_1 = 1 << ((63 - (number_rows as u64).leading_zeros()).div_ceil(2));
-	let dimension_2 = number_rows.div_ceil(dimension_1);
-	println!("dimensions = {} {}", dimension_1, dimension_2);
-	println!("dimension = {}", dimension_1 * dimension_2);
-	let mut preprocessed_database =
-		vec![Plaintext::zero(Encoding::Poly, &par); dimension_1 * dimension_2];
-	(0..number_rows).for_each(|i| {
-		let mut serialized_plaintext = vec![0u8; number_elements_per_plaintext * elements_size];
-		for j in 0..number_elements_per_plaintext {
-			if let Some(pt) = database.get(j + i * number_elements_per_plaintext) {
-				serialized_plaintext[j * elements_size..(j + 1) * elements_size]
-					.copy_from_slice(&pt)
-			}
-		}
-		let pt_values = transcode_backward(&serialized_plaintext, par.plaintext().ilog2() as usize);
-		let pt = Plaintext::try_encode(&pt_values as &[u64], Encoding::Poly, &par).unwrap();
-		preprocessed_database[i] = pt.clone();
-	});
-	Array2::from_shape_vec((dimension_1, dimension_2), preprocessed_database).unwrap()
-}
+use std::sync::Arc;
+use util::transcode_forward;
 
 fn main() -> Result<(), String> {
 	let database_size = 1 << 20;
