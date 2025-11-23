4f5d628ffbbc1f326dddfbe0cc023ae24dc72a4b
Tancrède Lepoint <tlepoint@users.noreply.github.com>
2025-11-22 21:33:50 -0500

    Upgrade and cleanup dependencies

--- crates/fhe-math/build.rs
+++
@@ -1,6 +0,0 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    // prost_build::compile_protos(&["src/proto/rq.proto"], &["src/proto"])?;
-    Ok(())
-}
--- crates/fhe/build.rs
+++
@@ -1,7 +0,0 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    // Generate the proto files.
-    // prost_build::compile_protos(&["src/proto/bfv.proto"], &["src/proto"])?;
-    Ok(())
-}
