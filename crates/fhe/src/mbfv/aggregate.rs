3dc5b0175124d7f7ac5870224039487a6407774d
Tancrède Lepoint <tlepoint@users.noreply.github.com>
2026-01-11 19:18:38 -0500

    Mark aggregate blanket impls as do_not_recommend (#344)

--- crates/fhe/src/mbfv/aggregate.rs
+++ crates/fhe/src/mbfv/aggregate.rs
@@ -8,6 +8,7 @@ pub trait Aggregate<S>: Sized {
         T: IntoIterator<Item = S>;
 }
 
+#[diagnostic::do_not_recommend]
 impl<S, A> Aggregate<Result<S>> for A
 where
     A: Aggregate<S>,
@@ -35,6 +36,7 @@ pub trait AggregateIter {
         A: Aggregate<Self::Share>;
 }
 
+#[diagnostic::do_not_recommend]
 impl<I: Iterator<Item = S>, S> AggregateIter for I {
     type Share = S;
 
@@ -43,5 +45,37 @@ impl<I: Iterator<Item = S>, S> AggregateIter for I {
         A: Aggregate<Self::Share>,
     {
         Aggregate::from_shares(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Aggregate, AggregateIter};
+    use crate::errors::Result;
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct Sum(u64);
+
+    impl Aggregate<u64> for Sum {
+        fn from_shares<T>(iter: T) -> Result<Self>
+        where
+            T: IntoIterator<Item = u64>,
+        {
+            Ok(Sum(iter.into_iter().sum()))
+        }
+    }
+
+    #[test]
+    fn aggregate_iter_collects_shares() -> Result<()> {
+        let sum = vec![1u64, 2, 3].into_iter().aggregate::<Sum>()?;
+        assert_eq!(sum, Sum(6));
+        Ok(())
+    }
+
+    #[test]
+    fn aggregate_result_flattens_shares() -> Result<()> {
+        let sum = <Sum as Aggregate<Result<u64>>>::from_shares(vec![Ok(1u64), Ok(2), Ok(3)])?;
+        assert_eq!(sum, Sum(6));
+        Ok(())
     }
 }
